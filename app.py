809c90ebc0e9953af62953912f6de7812ed21add
Toma_san <taramis637@gmail.com>
2022-01-03 23:53:59 +0000

    Finished app

--- app.py
+++ app.py
@@ -1,25 +1,45 @@
-from flask import Flask, request, render_template, send_from_directory
-# from functions import ...
+from flask import Flask, request, render_template, send_from_directory, abort
+from functions import *
 
 POST_PATH = "posts.json"
+data = get_json(POST_PATH)
 UPLOAD_FOLDER = "uploads/images"
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 
 
 @app.route("/")
 def page_index():
-    pass
+    tags = get_tags(data)
+    return render_template("index.html", tags=sorted(tags))
 
 
 @app.route("/tag")
 def page_tag():
-    pass
+    tag = request.args.get("tag")
+    if tag:
+        posts = get_post_by_tag(data, tag)
+        return render_template("post_by_tag.html", tag=tag, posts=posts)
+    return abort(400)
 
 
 @app.route("/post", methods=["GET", "POST"])
 def page_post_create():
-    pass
+    if request.method == "GET":
+        return render_template("post_form.html")
+
+    picture = request.files.get("picture")
+    content = request.form.get("content")
+    if not picture or not content:
+        return f"Ошибка загрузки!"
+
+    if request.method == "POST":
+        path = f"{UPLOAD_FOLDER}/{picture.filename}"
+        post = {"pic": f"/{path}", "content": content}
+        picture.save(path)
+        get_post(data, post, POST_PATH)
+        return render_template("post_uploaded.html", post=post)
 
 
 @app.route("/uploads/<path:path>")
@@ -27,5 +47,5 @@ def static_dir(path):
     return send_from_directory("uploads", path)
 
 
-app.run()
-
+if __name__ == "__main__":
+    app.run(debug=True)
--- functions.py
+++ functions.py
@@ -1,1 +1,32 @@
 import json
+
+
+def get_json(file):
+    with open(file, "r", encoding="utf=8") as f:
+        return json.load(f)
+
+
+def get_tags(data):
+    tag_list = set()
+    for posts in data:
+        contents = posts["content"]
+        cont = contents.split(" ")
+
+        for tag in cont:
+            if tag.startswith("#"):
+                tag_list.add(tag[1:])
+    return tag_list
+
+
+def get_post_by_tag(data, tag):
+    posts = []
+    for post in data:
+        if f"#{tag}" in post["content"]:
+            posts.append(post)
+    return posts
+
+
+def get_post(data, post, file):
+    data.append(post)
+    with open(file, "w", encoding="utf=8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=4, sort_keys=True)
